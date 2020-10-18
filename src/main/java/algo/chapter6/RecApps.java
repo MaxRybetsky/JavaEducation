099aa8955924d1227ee397c7daa9a850fe032047
MaxRybetsky <0SCRcm#%>
2020-10-18 14:21:50 +0300

    Chapter 6

--- src/main/java/algo/chapter6/RecApps.java
+++ src/main/java/algo/chapter6/RecApps.java
@@ -1,13 +1,22 @@
 package algo.chapter6;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RecApps {
+    static char[] a = {'A', 'B', 'C', 'D', 'E'};
+    static List<Character> s = new ArrayList<>();
+    static int kk;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
-        System.out.println(x + "^" + y + " = " + power(x, y));
+        System.out.println(x + "^" + y + " = " + power(x, y));*/
+
+        kk = 3;
+        combi(5, kk, ' ');
     }
 
     public static int power(int x, int y) {
@@ -17,5 +26,26 @@ public class RecApps {
             int m = y % 2 == 0 ? 1 : x;
             return m * power(x * x, y / 2);
         }
+    }
+
+    public static void combi(int n, int k, char c) {
+        if (c != ' ') {
+            s.add(c);
+            if(s.size() == kk) {
+                System.out.println(s);
+                s.remove(s.size() - 1);
+            }
+        }
+        if (n == 0 || k == 0 || k > n) {
+            return;
+        }
+        if (n == 1 && k == 1) {
+            s.add(a[a.length - n]);
+            System.out.println(s);
+
+            return;
+        }
+        combi(n - 1, k - 1, a[a.length - n]);
+        combi(n - 1, k, ' ');
     }
 }
