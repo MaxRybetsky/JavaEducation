149cb9a4cf973fa12f4c714009892eeddc65dfb8
MaxRybetsky <stsdewar@ya.ru>
2020-10-30 17:20:18 +0300

    Socket practise

---
+++ src/main/java/examples/sockets/threaded/ThreadedEchoServer.java
@@ -0,0 +1,56 @@
+package examples.sockets.threaded;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ThreadedEchoServer {
+    public static void main(String[] args) {
+        try(ServerSocket s = new ServerSocket(8189)) {
+            int i = 1;
+            while(true) {
+                Socket incoming = s.accept();
+                System.out.println("Spawning " + i);
+                Runnable r = new ThreadedEchoHandler(incoming);
+                Thread t = new Thread(r);
+                t.start();
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadedEchoHandler implements Runnable {
+    private final Socket incoming;
+
+    public ThreadedEchoHandler(Socket incoming) {
+        this.incoming = incoming;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream inStream = incoming.getInputStream();
+             OutputStream outStream = incoming.getOutputStream();
+             Scanner in = new Scanner(inStream, StandardCharsets.UTF_8);
+             PrintWriter out = new PrintWriter(
+                     new OutputStreamWriter(outStream,
+                             StandardCharsets.UTF_8),
+                     true)) {
+            out.println("Hello! Enter BYE to exit.");
+            boolean done = false;
+            while (!done && in.hasNextLine()) {
+                String line = in.nextLine();
+                out.println("Echo: " + line);
+                if (line.trim().equals("BYE")) {
+                    done = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
