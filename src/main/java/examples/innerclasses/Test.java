fceec9a59ee616e07cda4906efab7bdf3fbb20e8
MaxRybetsky <stsdewar@ya.ru>
2021-01-01 18:46:48 +0300

    Type of classes in Java

--- src/main/java/examples/innerclasses/Test.java
+++ src/main/java/examples/innerclasses/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Electrocar electrocar = new Electrocar(1);
         electrocar.start();
+
         Electrocar.Battery battery = new Electrocar.Battery();
         battery.charge();
     }
