c6c4d76352fd7db3055eb74ce1bba66cb4e4f61e
MaxRybetsky <stsdewar@ya.ru>
2021-02-24 19:16:36 +0300

    Experiments with lists

--- src/main/java/examples/other/A.java
+++ src/main/java/examples/other/A.java
@@ -1,8 +1,34 @@
 package examples.other;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class A implements Inter {
     @Override
     public void meth() {
         System.out.println("meth in A");
+    }
+
+    public static void main(String[] args) {
+        O[] ar = {new O(), new O(), new O(), new O()};
+        System.out.println(Arrays.toString(ar));
+        for (O elem : ar) {
+            elem.per = 14;
+        }
+        System.out.println(Arrays.toString(ar));
+        List<Integer> i = new ArrayList<>();
+        i.add(1);
+        i.add(1,2);
+        System.out.println(i);
+    }
+}
+
+class O {
+    public int per = 13;
+
+    @Override
+    public String toString() {
+        return String.valueOf(per);
     }
 }
